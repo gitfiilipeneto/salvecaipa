7f35126506d1de3f48565365f2b2c1408612730d
Filipe <filipenetopires@hotmail.com>
2020-11-26 17:01:49 -0300

    home

--- src/components/LandingHome.jsx
+++ src/components/LandingHome.jsx
@@ -1,24 +1,36 @@
 import react from "react"
 import styled from 'styled-components'
+import logo from '../images/logosalve.png'
 
-const Container = styled.div({
-    width:"100vw",
-    maxWidth:"100%",
-    height:"100vh",
-    maxHeight:"100%",
-    margin: "null",
+const Container = styled.div`
+    width:100vw;
+    max-width:100%;
+    height:100vh;
+    maxHeight:100%;
+    margin: unset;
 
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
-})
+`
 
+const StyledImage = styled.img`
+    
+    margin: 5px;
+    width:450px;
+    height: auto;
+    @media(max-width: 780px){
+        width:280px;
+    }
+`
 
 
 const Home = () => {
     return(
         <Container>
+            <StyledImage src = {logo}/>
             <p> Home </p>
         </Container>
     )
