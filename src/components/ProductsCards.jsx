6f549126d45e3d8f8edd3c1b1669d29d6d20984f
Filipe Neto <filipenetopires@hotmail.com>
2021-01-08 20:35:34 -0300

    Add to card, sabores, especiarias e botoes

--- src/components/ProductsCards.jsx
+++ src/components/ProductsCards.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import DefaultContainer from '../supp/DefaultContainer'
 import styled from 'styled-components'
+import { SABORES, ESPECIARIAS} from '../supp/CardsAssets'
 
 const Cardzin = styled.div`
     border: solid 1px black;
     border-radius: 30px;
     margin: 5px;
     padding: 5px;
-    height: 300px;
+    height: auto;
 `
 const Styledh1 = styled.h1`
 
@@ -23,6 +24,33 @@ class Cards extends Component{
                     <h1>{this.props.title}</h1>
 
                     <h2>{this.props.description}</h2>
+
+                    <table>
+                        <tr>
+                            <th>Sabor</th>
+                            <th>teste</th>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                {SABORES.map((sabor, index) => 
+                                <li key = {index}>{sabor}</li>
+                                )}
+                            </td>
+
+                            <td>
+                                {SABORES.map((sabor, index) => 
+                                <li key = {index}><button>+</button></li>
+                                )}
+                            </td>       
+                        </tr>                        
+                    </table>
+
+                    <ul>
+                        <p>Especiarias</p>
+                            {ESPECIARIAS.map((especiaria,index) => 
+                            <li key={index}>{especiaria}</li>)}
+                    </ul>
                 </div>
             </Cardzin>
         )
---
+++ src/supp/CardsAssets.js
@@ -0,0 +1,18 @@
+const SABORES = [
+    "Morango",
+    "Limão",
+    "Maracujá",
+    "Abacaxi",
+    "Vinho com Limão",    
+]
+
+const ESPECIARIAS = [
+    "Hortelã",
+    "Gengibre",
+    "Pimenta",
+]
+
+export {
+    SABORES,
+    ESPECIARIAS
+}
